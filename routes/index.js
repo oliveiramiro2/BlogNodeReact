1eb0e1271222462b4f859a81c75e31c8a877fa16
oliveiramiro2 <ramiromares95@gmail.com>
2021-07-03 00:28:42 -0300

    add react ao blog

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+/*## Aqui chamamos os arquivos contidos na pasta routes ##*/
 router.use('/', require('./post'))
+router.use('/', require('./comment'))
 
 module.exports = router
